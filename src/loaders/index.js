d41c929b6d1e69fb74b482f6d311a5c199ea1230
Chris Patenaude <zermish@localhost.localdomain>
2020-07-03 09:41:57 -0700

    Uncomment authentication

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,6 +2,6 @@ const expressLoader = require('./expressServer')
 const auth0Loader = require('./auth0')
 
 module.exports = async app => {
-  // await auth0Loader(app);
+  await auth0Loader(app);
   await expressLoader(app);
 }
